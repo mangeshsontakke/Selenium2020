9df74e7e70676a93e5fa11948d58be3a7c046e4f
Mangesh <mangesh.sontake@gmail.com>
2020-05-01 22:31:39 +0530

    facebookLogin 1

---
+++ src/test/java/FacebookLogin.java
@@ -0,0 +1,16 @@
+
+public class FacebookLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumWebdriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		
+		
+		
+
+	}
+
+}
